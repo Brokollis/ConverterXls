27cb1d3048219c30df2b06ed15612b6a108e4054
whenipkmyrr <vitor22aguiar06@gmail.com>
2024-12-06 15:52:35 -0300

    fix: align route of files on tsConif

--- src/utils/AuthProvider.tsx
+++ src/utils/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import { IAuthProvider, IContext, IAuthUserData } from "../interfaces/IAuth";
 import { getUserLocalStorage, setUserLocalStorage, UseLoginRequest } from "../services/UseLoginRequest";
 
